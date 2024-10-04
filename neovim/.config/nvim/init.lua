4a7e290a55d4fe772ae10e07b1fb67ab7f4660ab
Ryan Wüest <ryan.wueest@protonmail.com>
2024-10-04 15:28:03 +0200

    Move to lazyvim

--- neovim/.config/nvim/init.lua
+++ neovim/.config/nvim/init.lua
@@ -1,1 +1,4 @@
+-- Enables the experimental lua module loader to improve startup time
+vim.loader.enable()
+
 require("config")
--- neovim/.config/nvim/lua/config/keymaps.lua
+++
@@ -1,3 +0,0 @@
-local keymap = vim.keymap -- for conciseness
-
-keymap.set("i", "<C-c>", "<ESC>", { desc = "Exit insert mode with ctrl c" })

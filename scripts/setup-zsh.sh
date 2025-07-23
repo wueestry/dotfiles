9f66d9f31bffd0bdf5242e4366fd4c30a7b760e2
Ryan Wüest <ryan.wueest@protonmail.com>
2025-07-23 16:57:08 +0200

    Remove jkolit config

--- scripts/setup-zsh.sh
+++ scripts/setup-zsh.sh
@@ -2,13 +2,21 @@
 
 echo "This script sets zsh as the default shell"
 
-{ # If distro is based on debian/ubuntu
+if command -v apt &>/dev/null; then
+  # If distro is based on debian/ubuntu
   sudo apt install zsh
   chsh -s $(which zsh)
-} || { # If distro is based on fedora
+elif command -v dnf &>/dev/null; then
+  # If distro is based on fedora
   sudo dnf install zsh
   sudo chsh $USER
-}
+elif command -v pacman &>/dev/null; then
+  # If distro is based on arch
+  sudo pacman -Sy zsh
+  chsh -s $(which zsh)
+else
+  echo "Distro unkown. Do installation manually."
+fi
 
 # Install starship
 curl -sS https://starship.rs/install.sh | sh

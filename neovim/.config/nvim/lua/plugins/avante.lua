38582a035ad4995fdb8cd2f1d8e156fa95829325
Ryan Wüest <ryan.wueest@protonmail.com>
2025-11-14 16:45:55 +0100

    Dotfile update neovim

--- neovim/.config/nvim/lua/plugins/avante.lua
+++ neovim/.config/nvim/lua/plugins/avante.lua
@@ -4,9 +4,7 @@ return {
     "yetone/avante.nvim",
     -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
     -- ⚠️ must add this setting! ! !
-    build = vim.fn.has("win32") ~= 0 and "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false"
-      or "make",
-    event = "VeryLazy",
+    event = { "BufReadPre", "BufNewFile" },
     version = false, -- Never set this value to "*"! Never!
     dependencies = {
       "nvim-lua/plenary.nvim",
@@ -15,9 +13,18 @@ return {
       "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
       "zbirenbaum/copilot.lua", -- for providers='copilot'
       "folke/snacks.nvim",
+      "nvim-telescope/telescope.nvim",
+      {
+        -- Make sure to set this up properly if you have lazy=true
+        "MeanderingProgrammer/render-markdown.nvim",
+        opts = {
+          file_types = { "markdown", "Avante" },
+        },
+        ft = { "markdown", "Avante" },
+      },
     },
-    event = { "BufReadPre", "BufNewFile" },
     opts = {
+      instructions_file = "AGENTS.md",
       provider = "copilot",
       providers = {
         copilot = {
@@ -25,7 +32,7 @@ return {
           timeout = 30000, -- Timeout in milliseconds
           extra_request_body = {
             temperature = 0.75,
-            max_tokens = 20480,
+            max_tokens = 20000,
           },
         },
         ollama = {
@@ -33,29 +40,25 @@ return {
           model = "qwen2.5-coder:14b",
         },
       },
-      input = {
-        provider = "snacks",
-        provider_opts = {
-          -- Additional snacks.input options
-          title = "Avante Input",
-          icon = " ",
-        },
-      },
       selector = {
         --- @alias avante.SelectorProvider "native" | "fzf_lua" | "mini_pick" | "snacks" | "telescope" | fun(selector: avante.ui.Selector): nil
         --- @type avante.SelectorProvider
-        provider = "fzf",
+        provider = "telescope",
         -- Options override for custom providers
         provider_opts = {},
       },
+      behaviour = {
+        auto_apply_diff_after_generation = false,
+        auto_approve_tool_permissions = false,
+      },
+      mappings = {
+        suggestion = {
+          accept = "<C-a>",
+          next = "<C-]>",
+          prev = "<C-[>",
+          dismiss = "<C-d>",
+        },
+      },
     },
-  },
-  {
-    -- Render Markdown: Enhanced markdown rendering for Avante and markdown files
-    "MeanderingProgrammer/render-markdown.nvim",
-    opts = {
-      file_types = { "markdown", "Avante" },
-    },
-    ft = { "markdown", "Avante" },
   },
 }
